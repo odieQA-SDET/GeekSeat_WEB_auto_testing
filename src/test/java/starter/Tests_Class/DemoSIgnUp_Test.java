076299556229cefb9d8cff8b8b96fb85707f6ccb
Odie <93814478+odieQA-SDET@users.noreply.github.com>
2022-03-26 12:27:26 +0700

    Update DemoSIgnUp_Test.java

--- src/test/java/starter/Tests_Class/DemoSIgnUp_Test.java
+++ src/test/java/starter/Tests_Class/DemoSIgnUp_Test.java
@@ -7,54 +7,5 @@ public class DemoSIgnUp_Test {
 
     public void openHomePage() {
         ProductViaCategotyObject.open();
-    }
-
-    public void clickedscrollmenu() {
-        ProductViaCategotyObject.openscrollmenunclick();
-    }
-
-    public void clickedmaincategory() {
-        ProductViaCategotyObject.clickedMaincategory();
-    }
-
-    public void clickedleftnavigation() {
-        ProductViaCategotyObject.ClickedLeftnavigation();
-    }
-
-    public void clickedallfilter() {
-        ProductViaCategotyObject.ClickedAllFilter();
-    }
-
-    public void clikcedscreensize() {
-        ProductViaCategotyObject.clickedScreensize();
-    }
-
-    public void clickedscreensizecontent() {
-        ProductViaCategotyObject.ClickedscreensizeContent();
-    }
-
-    public void clickedprice() {
-        ProductViaCategotyObject.clickedPrice();
-    }
-
-    public void clickedpricecontent() {
-        ProductViaCategotyObject.clickedPricecontent1();
-        ProductViaCategotyObject.clickedPricecontent2();
-    }
-
-    public void clickeditemlocation() {
-        ProductViaCategotyObject.Clickeditemlocation();
-    }
-
-    public void clikceditemloccontent() {
-        ProductViaCategotyObject.itemlocationcontent();
-    }
-
-    public void clickedapply() {
-        ProductViaCategotyObject.Clickedapply();
-    }
-
-    public void clickedverifytagapplied() {
-        ProductViaCategotyObject.Clickedverifytagapplied();
-    }
+    
 }
